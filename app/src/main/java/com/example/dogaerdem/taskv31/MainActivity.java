a28eb503c0c7d5f43466ccff8ed6a20981464703
dogaerdem <dogaerdemfani@gmail.com>
2015-11-05 13:53:30 +0200

    Changes

--- app/src/main/java/com/example/dogaerdem/taskv31/MainActivity.java
+++ app/src/main/java/com/example/dogaerdem/taskv31/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,14 +17,22 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
         IntentFilter filter1 = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
         registerReceiver(mBroadcastReceiver1, filter1);
+        filter1.addAction("android.net.conn.");
+        IntentFilter filter2 = new IntentFilter();
+        filter2.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(mBroadcastReceiver2, filter2);
     }
+
     private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
 
         @Override
@@ -45,16 +55,61 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("BroadcastActions", "Bluetooth Açık");
                     }
                     break;
+
+
+                    }
+                }
+            };
+
+    private final BroadcastReceiver mBroadcastReceiver2 = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Log.e("BroadcastActions", "Internet");
+            if(isInternetAvailable()){
+                if(button2 != null) {
+                    button2.setBackgroundColor(Color.GREEN);
+                    button2.setText("Internet Var");
+                }
+                Log.e("BroadcastActions", "Internet acik");
+
+            }
+            else{
+                if (button2 != null) {
+                    button2.setBackgroundColor(Color.RED);
+                    button2.setText("Internet Yok");
+                }
+                Log.e("BroadcastActions", "Internet Kapali");
             }
         }
     };
+
+
         @Override
         protected void onDestroy() {
             super.onDestroy();
-
             unregisterReceiver(mBroadcastReceiver1);
+            unregisterReceiver(mBroadcastReceiver2);
+        }
 
+    private boolean isInternetAvailable() {
+        boolean result=true;
+        try{
+            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            if (activeNetwork != null && activeNetwork.isConnected()) {
+                result= true;
+            }else {
+                result= false;
             }
         }
+        catch (Exception e){
+            result=false;
+        }
+        return result;
+    }
+
+}
 
 
